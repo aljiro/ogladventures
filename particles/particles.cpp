1f268ff52d9954a34b2fef10f4717ea574a65d14
aljiro <aljiro@gmail.com>
2019-07-31 00:03:14 +0100

    pogress into the particles

--- particles/particles.cpp
+++ particles/particles.cpp
@@ -1,13 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include <iostream>
 #include <GL/glew.h>
-
 #include <GLFW/glfw3.h>
-GLFWwindow* window;
-
 #include <glm/glm.hpp>
+
+using namespace std;
 using namespace glm;
+
+GLFWwindow* window;
 
 typedef struct{
 	float m;
@@ -22,74 +21,27 @@ typedef struct{
 	float t;
 } *ParticleSystem;
 
+int particleDims( ParticleSystem p ){
+	return ( 6 * p->n );
+}
+
+int particlesGetState( ParticleSystem p, float *dst ){
+	for( int i = 0; i < p->n; i++ ){
+		*(dst++) = p->p[i]->x[0];
+		*(dst++) = p->p[i]->x[1];
+		*(dst++) = p->p[i]->x[2];
+		*(dst++) = p->p[i]->v[0];
+		*(dst++) = p->p[i]->v[1];
+		*(dst++) = p->p[i]->v[2];
+	}
+}
+
+// Drawing routines
+
+
 int main( void )
 {
-	// Initialise GLFW
-	if( !glfwInit() )
-	{
-		fprintf( stderr, "Failed to initialize GLFW\n" );
-		getchar();
-		return -1;
-	}
-
-	glfwWindowHint(GLFW_SAMPLES, 4);
-	glfwWindowHint(GLFW_RESIZABLE,GL_FALSE);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	// Open a window and create its OpenGL context
-	window = glfwCreateWindow( 1024, 768, "Playground", NULL, NULL);
-	if( window == NULL ){
-		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
-		getchar();
-		glfwTerminate();
-		return -1;
-	}
-
-	glfwMakeContextCurrent(window);
-
-	GLuint VertexArrayID;
-	glGenVertexArrays(1, &VertexArrayID);
-	glBindVertexArray(VertexArrayID);
-
-	static const GLfloat g_vertex_buffer_data[] = { 
-		-1.0f, -1.0f, 0.0f,
-		 1.0f, -1.0f, 0.0f,
-		 0.0f,  1.0f, 0.0f,
-	};
-
-	// Initialize GLEW
-	if (glewInit() != GLEW_OK) {
-		fprintf(stderr, "Failed to initialize GLEW\n");
-		getchar();
-		glfwTerminate();
-		return -1;
-	}
-
-	// Ensure we can capture the escape key being pressed below
-	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-
-	// Dark blue background
-	glClearColor(0.0f, 0.3f, 0.4f, 0.0f);
-
-	do{
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		// Draw nothing, see you in tutorial 2 !
-
-		// Swap buffers
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-
-	} // Check if the ESC key was pressed or the window was closed
-	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-		   glfwWindowShouldClose(window) == 0 );
-
-	// Close OpenGL window and terminate GLFW
-	glfwTerminate();
-
+	
 	return 0;
 }
 
